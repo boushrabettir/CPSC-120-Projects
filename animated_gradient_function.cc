7e615044f1e91642818c318fa778816136c9ce08
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2022-12-14 19:47:40 -0800

    Create animated_gradient_function.cc

---
+++ animated_gradient_function.cc
@@ -0,0 +1,27 @@
+#include "animated_gradient_functions.h"
+
+#include <cmath>
+#include <iostream>
+
+bool HasMatchingFileExtension(const std::string& file_name,
+                              const std::string& extension) {
+    if (file_name.length() >= extension.length() &&
+      (file_name.compare((file_name.length() - extension.length()),
+                         extension.length(), extension)) == 0) {
+    return true;
+  }
+  
+  return false;
+}
+
+
+std::vector<double> BuildSineLookupTable(int image_width) {
+  std::vector<double> lookup_table;
+  double radian_step = M_PI / double(image_width);
+  for (int column = 0; column < image_width; column++){
+    double value = sin(radian_step * column);
+    lookup_table.push_back(value);
+  }
+
+  return lookup_table;
+}
